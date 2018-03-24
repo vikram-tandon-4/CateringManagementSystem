1543c6f4b8e6ff2613e32c30a55ce7baad0a0d3a
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-03-24 15:11:49 -0500

    Registeration Screen
    1. Added toolbar implementation
    2. navigation back icons added
    3. Spinner(Dropdown setup)

--- CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/RegisterationActivity.java
+++ CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/RegisterationActivity.java
@@ -1,10 +1,20 @@
 package team4.softwareengineering.com.cateringsystem.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import team4.softwareengineering.com.cateringsystem.R;
+import team4.softwareengineering.com.cateringsystem.adapter.SimpleSpinnerAdaptor;
 
 /**
  * Created by vikra on 3/24/2018.
@@ -12,9 +22,60 @@ import team4.softwareengineering.com.cateringsystem.R;
 
 public class RegisterationActivity extends AppCompatActivity {
 
+    private Context mContext;
+    private Spinner spinnerCategory;
+    private ArrayAdapter<String> simpleSpinnerAdaptor;
+    private Toolbar toolbar;
+    private TextView tvTbTitle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registeration);
+
+        mContext= this;
+
+        init();
+
+
+    }
+
+    private void init() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvTbTitle = (TextView) findViewById(R.id.tvTbTitle);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        tvTbTitle.setText(R.string.request_event);
+
+        spinnerCategory = (Spinner) findViewById(R.id.spinnerCategory);
+        setSystemUserCategory();
+    }
+
+    /*
+
+Adding categories to dropdown
+ */
+    private void setSystemUserCategory(){
+
+
+
+        ArrayList<String> systemUserCategory=new ArrayList<>();
+
+        systemUserCategory= new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.category_array)));
+
+
+        spinnerCategory.setSelection(0);
+        simpleSpinnerAdaptor = new SimpleSpinnerAdaptor(mContext, android.R.layout.simple_spinner_item, systemUserCategory);
+        simpleSpinnerAdaptor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(simpleSpinnerAdaptor);
+
     }
 }
