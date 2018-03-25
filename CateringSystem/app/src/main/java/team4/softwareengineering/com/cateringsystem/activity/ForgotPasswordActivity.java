b1a9aa0e86ed7376ece7b75017bf114c838b6cf0
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-03-24 20:20:37 -0500

    Forgot Password Activity
    1.Added toolbar
    2. Manifest entry of the activity

--- CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/ForgotPasswordActivity.java
+++ CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/ForgotPasswordActivity.java
@@ -1,8 +1,12 @@
 package team4.softwareengineering.com.cateringsystem.activity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import team4.softwareengineering.com.cateringsystem.R;
 
@@ -12,9 +16,33 @@ import team4.softwareengineering.com.cateringsystem.R;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    private Context mContext;
+    private Toolbar toolbar;
+    private TextView tvTbTitle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forgotpassword);
+
+        mContext= this;
+        init();
+    }
+
+    private void init() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvTbTitle = (TextView) findViewById(R.id.tvTbTitle);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        tvTbTitle.setText(R.string.forgot_password);
+
     }
 }
