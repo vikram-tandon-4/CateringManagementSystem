f2cb6381130e79cf476fbc5bd09e8c5a1529ac13
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-03-24 20:29:01 -0500

    Login Activity
    Added toolbar.

--- CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/LoginActivity.java
+++ CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/LoginActivity.java
@@ -1,15 +1,34 @@
 package team4.softwareengineering.com.cateringsystem.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import team4.softwareengineering.com.cateringsystem.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Context mContext;
+    private Toolbar toolbar;
+    private TextView tvTbTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+        mContext= this;
+        init();
+    }
+
+    private void init() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvTbTitle = (TextView) findViewById(R.id.tvTbTitle);
+
+        tvTbTitle.setText(R.string.login);
+
     }
 }
