21726a659f5f7252a132f119a8014a46510ce1a7
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-03-24 20:53:28 -0500

    Event Details Activity
    1. Added activity
    2. Added toolbar and scrollview

---
+++ CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/EventDetailsActivity.java
@@ -0,0 +1,72 @@
+package team4.softwareengineering.com.cateringsystem.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import team4.softwareengineering.com.cateringsystem.R;
+
+/**
+ * Created by vikra on 3/24/2018.
+ */
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    private Context mContext;
+    private Toolbar toolbar;
+    private TextView tvTbTitle;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_details);
+
+        mContext= this;
+        init();
+    }
+
+    private void init() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        tvTbTitle = (TextView) findViewById(R.id.tvTbTitle);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        tvTbTitle.setText(R.string.event_details);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.home);
+        toolbar.setOnMenuItemClickListener(new android.support.v7.widget.Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.logout:
+                        Toast.makeText(mContext, "Logout",Toast.LENGTH_LONG).show();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+
+    }
+}
