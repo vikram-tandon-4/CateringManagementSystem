b76945da119b6c1bf157b5fcd43b8203f3014806
PradeepRavichandran1811 <pradeep181195@gmail.com>
2018-04-28 20:27:08 -0500

    Splash Screen for the App

---
+++ CateringSystem/app/src/main/java/team4/softwareengineering/com/cateringsystem/activity/SplashScreenActivity.java
@@ -0,0 +1,45 @@
+package team4.softwareengineering.com.cateringsystem.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+import team4.softwareengineering.com.cateringsystem.R;
+
+/**
+ * Created by Prady on 4/28/2018.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity{
+
+    private static long SPLASH_TIME_OUT= 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
